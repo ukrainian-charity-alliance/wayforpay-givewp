640461fc5d613478c127b87c3b8c23c9798e3268
Dan Radion <dan.radion@gmail.com>
2026-07-19 20:32:42 +0200

    update to GPL3 license

--- wayforpay-givewp.php
+++ wayforpay-givewp.php
@@ -8,6 +8,8 @@
  * Requires Plugins: give
  * Author: Ukrainian Charity Alliance
  * Author URI: https://uba.com.ua
+ * License: GPL-3.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: wayforpay-givewp
  * Domain Path: /languages
  */
